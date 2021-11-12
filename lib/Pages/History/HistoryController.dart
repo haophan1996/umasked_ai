982b124b5416c8dc531bc706093bf451f565fde0
chobay <phanh5@wit.edu>
2021-11-12 02:32:37 -0500

    add api

--- lib/Pages/History/HistoryController.dart
+++ lib/Pages/History/HistoryController.dart
@@ -1,16 +1,51 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:get/get.dart';
+import 'package:dio_http/dio_http.dart';
+
 
 class HistoryController extends GetxController {
   late File imagePath;
+  var dio = Dio();
+  Map responseBody = {};
 
   @override
-  void onInit() {
+  Future<void> onInit() async {
     // TODO: implement onInit
     super.onInit();
     if (Get.arguments != null) {
       imagePath = Get.arguments[0];
-    } else
+      await processImage();
+    } else {
       Get.back();
+    }
+  }
+
+
+
+  Future processImage() async {
+    String imageB64 = base64Encode(imagePath.readAsBytesSync());
+
+
+
+    var headers = {
+      'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'host': '10.0.2.2:5000',
+    };
+
+    var body = {
+      'file' : imageB64
+    };
+
+    dio.options.headers = headers;
+    final response =  await dio.post('http://10.0.2.2:5000/predict',data: body);
+    responseBody = response.data;
+    update();
+
   }
 }
+
+
+/***server address
+ * http://10.0.2.2:5000/
+ ***/
--- lib/Pages/History/HistoryUI.dart
+++ lib/Pages/History/HistoryUI.dart
@@ -1,5 +1,7 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'HistoryController.dart';
 
@@ -11,21 +13,49 @@ class HistoryUI extends GetView<HistoryController> {
         title: const Text('Image processing'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Image.file(controller.imagePath),
-          Text(
-            'Detect 1 student confused',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: Get.textTheme.headline6!.fontSize),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20, top: 10),
+            child: Container(
+              alignment: Alignment.center,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.file(controller.imagePath),
+              ),
+            ),
           ),
-          Text(
-            'This will send image to backend to process ',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: Get.textTheme.headline6!.fontSize),
-          ),
-          const CupertinoActivityIndicator()
+          GetBuilder<HistoryController>(builder: (controller) {
+            if (controller.responseBody.isEmpty == true) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Processing\t\t\t',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: Get.textTheme.headline6!.fontSize)),
+                  const CupertinoActivityIndicator()
+                ],
+              );
+            } else {
+              return Column(
+                children: [
+                  Text(
+                    'Detected',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.redAccent,
+                        fontSize: Get.textTheme.bodyText1!.fontSize! + 5.0),
+                  ),
+                  Text(controller.responseBody.toString(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: Get.textTheme.bodyText1!.fontSize! + 3.0))
+                ],
+              );
+            }
+          }),
         ],
       ),
     );
